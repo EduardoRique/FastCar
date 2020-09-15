6b6496cfef82ba24e64b78b05209ba41a8eebc6c
Eduardo Rique <luiseduardorique@gmail.com>
2020-09-15 19:55:16 -0300

    Criando projeto

---
+++ vTest/src/test/java/com/vTest/VTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
