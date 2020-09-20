6db34dbb2570689a6f5f44cbaf251988d0feaefb
frankstark <franklinluiz2000@gmail.com>
2020-09-20 16:51:18 -0300

    entidades implementadas

---
+++ vTest/src/main/java/model/enviaRecebe.java
@@ -0,0 +1,23 @@
+package model;
+
+
+public class enviaRecebe {
+    //idDestinatario INT
+    private int idDestinatario;
+    //idMensagem INT
+    private int idMensagem;
+
+    public int getIddestinatario(){
+            return idDestinatario;
+    }
+    public void setIdDestinatario(int idDestinatario){
+            this.idDestinatario = idDestinatario;
+    }
+    
+    public int getMsg() {
+    	return this.idMensagem ;
+    }
+    public void setMsg(int idMsg) {
+    	this.idMensagem = idMsg;
+    }
+}
